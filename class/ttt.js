0e2050c8ec2cca8173b33515b0bf09766f31a1ab
sido <imbilusydney09@gmail.com>
2024-05-10 08:46:33 +0300

    implemented placeMove method in ttt class

--- class/ttt.js
+++ class/ttt.js
@@ -18,7 +18,12 @@ class TTT {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
+    // Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
+    Screen.addCommand('up', 'Up', this.cursor.up.bind(this.cursor));
+    Screen.addCommand('left', 'Left', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'Right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('down', 'Down', this.cursor.down.bind(this.cursor));
+    Screen.addCommand('return', 'Place move', this.placeMove.bind(this));
 
     Screen.render();
   }
@@ -27,6 +32,32 @@ class TTT {
   // static testCommand() {
   //   console.log("TEST COMMAND");
   // }
+
+
+  placeMove() {
+    const row = this.cursor.row;
+    const col = this.cursor.col;
+
+    // Check if the selected cell is empty
+    if (this.grid[row][col] === ' ') {
+      // Place the current player's mark
+      this.grid[row][col] = this.playerTurn;
+      Screen.setGrid(row, col, this.playerTurn);
+
+      // Switch turns
+      this.playerTurn = this.playerTurn === 'X' ? 'O' : 'X';
+
+      // Check for win or tie after placing the move
+      const winner = TTT.checkWin(this.grid);
+      if (winner) {
+        this.endGame(winner);
+      }
+    } else {
+      // Display a message if the cell is occupied
+      Screen.setMessage("This cell is already occupied. Try again!");
+      Screen.render();
+    }
+  }
 
   static checkWin(grid) {
 
